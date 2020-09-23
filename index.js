dadc2faeba185be749b296479a27725a0675f6c5
liaozge <50882424+liaozge@users.noreply.github.com>
2020-09-23 12:07:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get('/',(req,res)=>{
     res.send("hello heroku app ")
 })
 
-//app.listen(process.env.PORT||3000)
-app.listen(3000,()=>{console.log("servers start...port:3000")})
+app.listen(process.env.PORT||3000)
+//app.listen(3000,()=>{console.log("servers start...port:3000")})
